bb1f4bc753d3655d7adefa1b508adb20e5f7ddc9
parksumi <parksumi0127@gmail.com>
2024-05-16 20:11:31 +0900

    과제 완성

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const container = document.querySelector("#container");
 const box = document.querySelectorAll(".box");
 
 container.addEventListener("click", function (e) {
-  console.log(e.target.className);
   if (e.target.className === "box") {
     e.target.classList.add("clicked");
   }
@@ -15,4 +14,4 @@ function getSquare(number) {
 }
 
 const result = getSquare(5);
-console.log(result); // 25 출력 (5의 제곱)
+console.log(result);
